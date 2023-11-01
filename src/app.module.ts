363b2abf2a297eb629aa038f405c4ffc1360e3b5
franco22bellido <franco.bellido22@gmail.com>
2023-11-01 16:14:00 -0300

    última subida

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,18 +5,30 @@ import { CatsModule } from './cats/cats.module';
 import { BreedsModule } from './breeds/breeds.module';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [CatsModule
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true
+    }),
+    CatsModule
   ,TypeOrmModule.forRoot({
-    type: "mysql",
-    host: "localhost",
-    port: 3307,
-    username: "user_crud",
-    password: "root",
-    database: "db_crud",
+    type: "postgres",
+    host: process.env.POSTGRES_HOST,
+    port: parseInt( process.env.POSTGRES_PORT),
+    username:  process.env.POSTGRES_USERNAME,
+    password:  process.env.POSTGRES_PASSWORD,
+    database:  process.env.POSTGRES_DATABASE,
     autoLoadEntities: true,
-    synchronize: true
+    synchronize: true,
+    ssl: process.env.POSTGRES_SSL === "true",
+    extra: {
+      ssl: process.env.POSTGRES_SSL === "true"
+      ? {
+        rejectUnauthorized: false
+      }: null
+    }
   }), BreedsModule, UserModule, AuthModule
   ],
   controllers: [],
